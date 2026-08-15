4c6b55df061b4ed80b901c76587e8c0faebefdf1
Rector96 <peteroche53@gmail.com>
2026-08-15 22:39:34 +0100

    Update pay.$slug.tsx
    fix: remove bad text breaking pay page

--- src/routes/pay.$slug.tsx
+++ src/routes/pay.$slug.tsx
@@ -1,4 +1,4 @@
-RESTORE_FROM_LOCALimport { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { useEffect, useMemo, useRef, useState } from "react";
 import {
   AlertCircle,
